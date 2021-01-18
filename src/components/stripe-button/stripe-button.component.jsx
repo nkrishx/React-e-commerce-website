3d1e4b9f7c7a0966840155def6daa6ad574f997f
nkrishx <naveen.gowda.rm@gmail.com>
2021-01-18 20:26:59 +0000

    added stripe payment component

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51IB3sDDEO7gnoRyONjTDslPz4JHZ2AMcdZciiT6ydinBBMPRI1JOXeYOwsQ1i8NFcugfZ23v6CSlPFSsETl242RT00xS7uhpgy';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
